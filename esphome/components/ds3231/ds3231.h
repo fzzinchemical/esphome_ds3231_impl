4d9080818b0a666accfb27d4936ad71b94274bf2
fzzinchemical <mathys162002@gmail.com>
2024-10-21 18:03:42 +0200

    Refactor DS3231Component struct and union

--- esphome/components/ds3231/ds3231.h
+++ esphome/components/ds3231/ds3231.h
@@ -40,133 +40,131 @@ class DS3231Component : public time::RealTimeClock, public i2c::I2CDevice {
 
   bool read_temperature_();
 
-  union DS3231Reg {
-    struct RTC {
-      // 00h (Seconds)
-      uint8_t second : 4;
-      uint8_t second_10 : 3;
-      bool unused_0 : 1;
+  struct RTC {
+    // 00h (Seconds)
+    uint8_t second : 4;
+    uint8_t second_10 : 3;
+    bool unused_0 : 1;
 
-      // 01h (Minutes)
-      uint8_t minute : 4;
-      uint8_t minute_10 : 3;
-      bool unused_1 : 1;
+    // 01h (Minutes)
+    uint8_t minute : 4;
+    uint8_t minute_10 : 3;
+    bool unused_1 : 1;
 
-      // 02h (Hours)
-      uint8_t hour : 4;
-      uint8_t hour_10 : 1;
-      uint8_t hour_20 : 1;
-      uint8_t hour_format_12_24 : 1;  // 0: 24-hour, 1: 12-hour format
-      bool unused_2 : 1;
+    // 02h (Hours)
+    uint8_t hour : 4;
+    uint8_t hour_10 : 1;
+    uint8_t hour_20 : 1;
+    uint8_t hour_format_12_24 : 1;  // 0: 24-hour, 1: 12-hour format
+    bool unused_2 : 1;
 
-      // 03h (Day)
-      uint8_t weekday : 3;
-      uint8_t unused_3 : 5;
+    // 03h (Day)
+    uint8_t weekday : 3;
+    uint8_t unused_3 : 5;
 
-      // 04h (Date)
-      uint8_t day : 4;
-      uint8_t day_10 : 2;
-      uint8_t unused_4 : 2;
+    // 04h (Date)
+    uint8_t day : 4;
+    uint8_t day_10 : 2;
+    uint8_t unused_4 : 2;
 
-      // 05h (Month/Century)
-      uint8_t month : 4;
-      bool month_10 : 1;
-      uint8_t unused_5 : 2;
-      bool century : 1;
+    // 05h (Month/Century)
+    uint8_t month : 4;
+    bool month_10 : 1;
+    uint8_t unused_5 : 2;
+    bool century : 1;
 
-      // 06h (Year)
-      uint8_t year : 4;
-      uint8_t year_10 : 4;
-    } rtc;
+    // 06h (Year)
+    uint8_t year : 4;
+    uint8_t year_10 : 4;
+  } rtc_;
 
-    // Alarm Registers 0x7 and 0xB IF ALARM2 is used, there will be no seconds!
+  // Alarm Registers 0x7 and 0xB IF ALARM2 is used, there will be no seconds!
 
-    struct Alarm {
-      union {
-        struct {
-          // 07h (Alarm 1 Seconds)
-          uint8_t a_second : 4;
-          uint8_t a_second_10 : 3;
-          bool a_m1 : 1;
+  struct Alarm {
+    union {
+      struct {
+        // 07h (Alarm 1 Seconds)
+        uint8_t a_second : 4;
+        uint8_t a_second_10 : 3;
+        bool a_m1 : 1;
 
-          // 08h (Alarm 1 Minutes)
-          uint8_t a_minute : 4;
-          uint8_t a_minute_10 : 3;
-          bool a_m2 : 1;
+        // 08h (Alarm 1 Minutes)
+        uint8_t a_minute : 4;
+        uint8_t a_minute_10 : 3;
+        bool a_m2 : 1;
 
-          // 09h (Alarm 1 Hours)
-          uint8_t a_hour : 4;
-          uint8_t a_hour_10 : 1;
-          bool a_hour_20 : 1;
-          uint8_t a_hour_format_12_24 : 1;
-          bool a_m3 : 1;
+        // 09h (Alarm 1 Hours)
+        uint8_t a_hour : 4;
+        uint8_t a_hour_10 : 1;
+        bool a_hour_20 : 1;
+        uint8_t a_hour_format_12_24 : 1;
+        bool a_m3 : 1;
 
-          // 0Ah (Alarm 1 Day/Date)
-          uint8_t a_date : 4;
-          uint8_t a_date_10 : 2;
-          bool a_dy_dt : 1;
-          bool a_m4 : 1;
-        } alarm1;
-        struct {
-          // 08h (Alarm 1 Minutes)
-          uint8_t a_minute : 4;
-          uint8_t a_minute_10 : 3;
-          bool a_m2 : 1;
+        // 0Ah (Alarm 1 Day/Date)
+        uint8_t a_date : 4;
+        uint8_t a_date_10 : 2;
+        bool a_dy_dt : 1;
+        bool a_m4 : 1;
+      } alarm1;
+      struct {
+        // 08h (Alarm 1 Minutes)
+        uint8_t a_minute : 4;
+        uint8_t a_minute_10 : 3;
+        bool a_m2 : 1;
 
-          // 09h (Alarm 1 Hours)
-          uint8_t a_hour : 4;
-          uint8_t a_hour_10 : 1;
-          bool a_hour_20 : 1;
-          uint8_t a_hour_format_12_24 : 1;
-          bool a_m3 : 1;
+        // 09h (Alarm 1 Hours)
+        uint8_t a_hour : 4;
+        uint8_t a_hour_10 : 1;
+        bool a_hour_20 : 1;
+        uint8_t a_hour_format_12_24 : 1;
+        bool a_m3 : 1;
 
-          // 0Ah (Alarm 1 Day/Date)
-          uint8_t a_date : 4;
-          uint8_t a_date_10 : 2;
-          bool a_dy_dt : 1;
-          bool a_m4 : 1;
-        } alarm2;
-      };
+        // 0Ah (Alarm 1 Day/Date)
+        uint8_t a_date : 4;
+        uint8_t a_date_10 : 2;
+        bool a_dy_dt : 1;
+        bool a_m4 : 1;
+      } alarm2;
     };
+  };
 
-    // Special-Purpose Registers
-    // 0Eh (Control Register)
-    struct Special {
-      bool a1le : 1;
-      bool a2le : 1;
-      bool intcn : 1;
-      bool rs1 : 1;
-      bool rs2 : 1;
-      bool conv : 1;
-      bool bbsqw : 1;
-      bool eosc : 1;
+  // Special-Purpose Registers
+  // 0Eh (Control Register)
+  struct Special {
+    bool a1le : 1;
+    bool a2le : 1;
+    bool intcn : 1;
+    bool rs1 : 1;
+    bool rs2 : 1;
+    bool conv : 1;
+    bool bbsqw : 1;
+    bool eosc : 1;
 
-      // 0Fh (Status Register)
-      bool a1f : 1;
-      bool a2f : 1;
-      bool bsy : 1;
-      bool en32khz : 1;
-      uint8_t unused_6 : 3;
-      bool osf : 1;
-    } special;
+    // 0Fh (Status Register)
+    bool a1f : 1;
+    bool a2f : 1;
+    bool bsy : 1;
+    bool en32khz : 1;
+    uint8_t unused_6 : 3;
+    bool osf : 1;
+  } special;
 
-    // 10h (Aging Offset)
-    int8_t aging_offset;
+  // 10h (Aging Offset)
+  int8_t aging_offset;
 
-    struct Temperature {
-      // 11h (Temperature MSB)
-      int8_t temperature_msb;
+  struct Temperature {
+    // 11h (Temperature MSB)
+    int8_t temperature_msb;
 
-      // 12h (Temperature LSB)
-      uint8_t unused_7 : 6;
-      uint8_t temperature_lsb : 2;
-    } temperature;
-    mutable uint8_t raw_rtc[sizeof(rtc)];
-    mutable uint8_t raw_alarm1[sizeof(Alarm::alarm1)];
-    mutable uint8_t raw_alarm2[sizeof(Alarm::alarm2)];
-    mutable uint8_t raw_special[sizeof(special)];
-    mutable uint8_t raw_temperature[sizeof(temperature)];
-  } ds3231_;
+    // 12h (Temperature LSB)
+    uint8_t unused_7 : 6;
+    uint8_t temperature_lsb : 2;
+  } temperature;
+  uint8_t raw_rtc[sizeof(rtc_)];
+  uint8_t raw_alarm1[sizeof(Alarm::alarm1)];
+  uint8_t raw_alarm2[sizeof(Alarm::alarm2)];
+  uint8_t raw_special[sizeof(special)];
+  uint8_t raw_temperature[sizeof(temperature)];
 };
 template<typename... Ts> class WriteAction : public Action<Ts...>, public Parented<DS3231Component> {
  public:
